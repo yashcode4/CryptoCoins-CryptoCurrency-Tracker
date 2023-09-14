43dd9c5eae4f73b5d8949f6895e465253cd1a0bd
yashcode4 <yash124987@gmail.com>
2023-09-14 21:43:31 +0530

    changes

--- App.js
+++ App.js
@@ -1,16 +1,13 @@
 const express = require('express')
 const app = express()
+const port = 3000
 const path = require("path") // For join the path.
-const dotenv = require("dotenv"); // Importing dotenv
-
-dotenv.config({
-  path: "./.env"
-})
 
 function resData() {
   req.body.coinName;
 }
-app.use('/public', express.static('public')); // The app.use() function is used to mount the specified middleware function(s) like (express.static) at the path which is being specified like (/public).
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use('/public', express.static('public')); // The app.use() function is used to mount the specified middleware function(s) like (express.static) at the path which is being specified like (/public).
  // "/public" - use this path for express.static('public') middleware function. Express can use "public" folder for every static files.
 
 // get to the user.
@@ -25,8 +22,8 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html')); // For sending file, we have to join current directory to the views directory and send its "index.html" file.
 })
 
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on http://localhost:${process.env.PORT}`)
+app.listen(port, () => {
+  console.log(`Example app listening on http://localhost:${port}`)
 })
 
 
